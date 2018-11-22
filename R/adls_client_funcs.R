f28173adc4f75fcdb43ae7db9c2c26ad4b2b08d0
hong-revo <hongooi@microsoft.com>
2018-11-22 12:09:41 +1100

    1st cut, adls file ops

--- R/adls_client_funcs.R
+++ R/adls_client_funcs.R
@@ -115,7 +115,7 @@ delete_adls_filesystem <- function(endpoint, ...)
 #' @rdname adls_filesystem
 #' @export
 delete_adls_filesystem.character <- function(endpoint, key=NULL, sas=NULL,
-                                             api_version=getOption("azure_storage_api_version"),
+                                             api_version=getOption("azure_adls_api_version"),
                                              ...)
 {
     endp <- generate_endpoint_container(endpoint, key, sas, api_version)
@@ -144,3 +144,75 @@ delete_adls_filesystem.adls_endpoint <- function(endpoint, name, confirm=TRUE, .
     obj <- adls_filesystem(endpoint, name)
     do_container_op(obj, options=list(resource="filesystem"), http_verb="DELETE")
 }
+
+
+
+
+#' @rdname adls
+#' @export
+list_adls_files <- function(filesystem, dir="/", info=c("all", "name"),
+                            recursive=FALSE)
+{
+    info <- match.arg(info)
+
+    opts <- list(recursive=tolower(as.character(recursive)), resource="filesystem")
+    opts <- c(opts, directory=as.character(dir))
+
+    lst <- do_container_op(filesystem, "", options=opts)
+    lst$paths
+}
+
+
+#' @rdname adls
+#' @export
+upload_adls_file <- function(filesystem, src, dest, blocksize=2^24, lease=NULL)
+{
+    content_type <- mime::guess_type(src)
+    headers <- list("x-ms-content-type"=content_type)
+    if(!is.null(lease))
+        headers[["x-ms-lease-id"]] <- as.character(lease)
+
+    con <- if(inherits(src, "textConnection"))
+        rawConnection(charToRaw(paste0(readLines(src), collapse="\n")))
+    else file(src, open="rb")
+    on.exit(close(con))
+
+    opts <- list(resource="file")
+    do_container_op(filesystem, dest, options=opts, headers=headers, http_verb="PUT")
+    message("File created")
+
+    body <- readBin(con, "raw", blocksize)
+    bodylen <- length(body)
+    headers[["content-length"]] <- sprintf("%.0f", bodylen)
+    opts <- list(resource="file", action="append")
+    do_container_op(filesystem, dest, options=opts, headers=headers, body=body, http_verb="PATCH")
+}
+
+
+
+
+#' @rdname file
+#' @export
+create_adls_dir <- function(filesystem, dir)
+{
+    do_container_op(filesystem, dir, options=list(resource="directory"), http_verb="PUT")
+}
+
+
+#' @rdname file
+#' @export
+delete_adls_dir <- function(filesystem, dir, confirm=TRUE, recursive=FALSE)
+{
+    if(confirm && interactive())
+    {
+        endp <- filesystem$endpoint
+        path <- paste0(endp$url, filesystem$name, "/", dir)
+        yn <- readline(paste0("Are you sure you really want to delete directory '", path, "'? (y/N) "))
+        if(tolower(substr(yn, 1, 1)) != "y")
+            return(invisible(NULL))
+    }
+
+    opts <- list(recursive=tolower(as.character(recursive)))
+    do_container_op(filesystem, dir, options=opts, http_verb="DELETE")
+}
+
